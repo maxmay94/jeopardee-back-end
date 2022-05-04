9de98b3244d0f21929cbb9d93458a28bd6860ac3
maxmay94 <maxmay94@gmail.com>
2022-05-04 10:41:13 -0400

    add question model

---
+++ models/question.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const questionSchema = new Schema(
+  {
+    question: {
+      type: String,
+      required: true
+    },
+    answer: {
+      type: String,
+      required: true
+    },
+    category: {
+      type: String,
+      required: true
+    },
+    difficulty: {
+      type: Number,
+      required: true,
+      enum: [
+        200,
+        400,
+        600,
+        800,
+        1000,
+        1200,
+        1600,
+        2000
+      ]
+    },
+  },
+  { timestamps: true }
+)
+
+const Question = mongoose.model("Question", questionSchema)
+
+export { Question }
