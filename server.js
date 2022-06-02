96e2bd9cfd48c1e3c6d555c0a120b84e62df34fa
maxmay94 <maxmay94@gmail.com>
2022-06-02 12:25:40 -0400

    walking back old cors fix attemmpts and cleaning up

--- server.js
+++ server.js
@@ -12,25 +12,26 @@ import('./config/database.js')
 const app = express()
 
 app.use(cors({
-  origin: '*',
-  methods: 'GET', //! TESTING JUST GET used to be *
-  credentials: 'false'
+  // origin: '*',
+  // methods: 'GET', //! TESTING JUST GET used to be *
+  // credentials: 'false'
 }))
 
 //------------------- TEST ZONE -------------------
-app.options('*', cors())
-app.get('/api/questions', function (req, res, next) {
-  res.json({ msg: 'This is CORS-enabled for all origins!' })
-})
-app.get('/api/profiles', function (req, res, next) {
-  res.json({ msg: 'This is CORS-enabled for all origins!' })
-})
-app.get('/api/auth', function (req, res, next) {
-  res.json({ msg: 'This is CORS-enabled for all origins!' })
-})
-app.get('/', function (req, res, next) {
-  res.json({ msg: 'This is CORS-enabled for all origins!' })
-})
+// app.options('*', cors())
+// app.get('/api/questions', function (req, res, next) {
+//   res.json({ msg: 'This is CORS-enabled for all origins!' })
+// })
+// app.get('/api/profiles', function (req, res, next) {
+//   res.json({ msg: 'This is CORS-enabled for all origins!' })
+// })
+// app.get('/api/auth', function (req, res, next) {
+//   res.json({ msg: 'This is CORS-enabled for all origins!' })
+// })
+// app.get('/', function (req, res, next) {
+//   res.json({ msg: 'This is CORS-enabled for all origins!' })
+// })
+
 //------------------- TEST ZONE -------------------
 
 app.use(logger('dev'))
@@ -39,10 +40,6 @@ app.use(express.json())
 app.use('/api/profiles', profilesRouter)
 app.use('/api/auth', authRouter)
 app.use('/api/questions', questionsRouter)
-
-//! app.use(function (req, res, next) {
-//!   res.status(404).json({ err: "Not found -- this is in server.js line 34" })
-//! })
 
 app.use(function (err, req, res, next) {
   res.status(err.status || 500).json({ err: err.message + " server.js line 38" })
